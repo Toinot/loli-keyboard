deda8ed94e469dd9e8989312671f041c3ac8672f
Toinot <60398094+Toinot@users.noreply.github.com>
2021-09-20 16:52:51 +0200

    Add files via upload

---
+++ LOLI Keyboard/LOLI.py
@@ -0,0 +1,8 @@
+# Créé par toinot.gury, le 20/09/2021 en Python 3.7
+import eel
+
+eel.init('web')
+
+geometry={'size': (500, 600)}
+
+eel.start('index.html', size=(500, 600), block=True)
---
+++ LOLI Keyboard/var.py
@@ -0,0 +1,2 @@
+# Créé par toinot.gury, le 20/09/2021 en Python 3.7
+f8 = "P:\Downloads\yamete-kudasai-meme (mp3cut.net).mp3"
